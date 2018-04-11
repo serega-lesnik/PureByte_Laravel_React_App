002cbb3ad17f198fcc184da2067511624698c3a8
Sergii Che <s.cherednichenko@ossystem.com.ua>
2018-04-11 14:26:12 +0300

    create the api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/**
+**Basic Routes for a RESTful service:
+**Route::get($uri, $callback);
+**Route::post($uri, $callback);
+**Route::put($uri, $callback);
+**Route::delete($uri, $callback);
+**
+*/
+Route::get('events', 'EventsController@index');
+
+Route::get('events/{event}', 'EventsController@show');
+
+Route::post('events','EventsController@store');
+
+Route::put('events/{event}','EventsController@update');
+
+Route::delete('events/{event}', 'EventsController@delete');
